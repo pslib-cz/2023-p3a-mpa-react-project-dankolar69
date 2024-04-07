ebc4c626f4b4a046e104cf3aeb4638bfe830d76d
Daniel Kolář <daniel1142@seznam.cz>
2024-04-07 20:13:38 +0200

    name

--- my-vue-app/src/pages/MainMenu.tsx
+++ my-vue-app/src/pages/MainMenu.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import "../styles/Main.css";
 import { Link } from "react-router-dom";
 
+
  const MainMenu: React.FC = () => {
     return (
+        
         <div className="container">
+            <h1 className="title">Space Shooter</h1>
             <Link to="/gameplay" className="link"> 
                 <button className="button">
                     <a>Start Game</a>
